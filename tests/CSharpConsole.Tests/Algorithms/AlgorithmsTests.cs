df461f3e3edf4d541ee2d04a6c9915e2ec5c902b
tony <antonel.pazargic@gmail.com>
2024-03-18 19:19:16 +0200

    Algorithms: Factorial

---
+++ tests/CSharpConsole.Tests/Algorithms/AlgorithmsTests.cs
@@ -0,0 +1,43 @@
+using Xunit;
+
+namespace CSharpConsole.Tests.Algorithms;
+
+
+public class AlgorithmsTests
+{
+    [Fact]
+    public void TestArithmeticMean()
+    {
+        Console.WriteLine("TestArithmeticMean");
+        Console.Write("n = ");
+        int.TryParse(Console.ReadLine(), out var n);
+        if (n == 0)
+        {
+            Console.WriteLine("No result.");
+        } else
+        {
+            var sum = 0.0;
+            var i = 0;
+            do
+            {
+                Console.Write("x = ");
+                int.TryParse(Console.ReadLine(), out var x);
+                sum += x;
+                i++;
+            }
+            while (i != n);
+            
+            var result = sum / n;
+            Console.WriteLine($"Arithmetic mean = {result}");
+        }
+    }
+
+    [Fact]
+    public void TestFactorial()
+    {
+        int Factorial(int n, int acc = 1) => 
+            n == 0 ? acc : Factorial(n - 1, acc * n);
+        
+        Assert.Equal(6, Factorial(3));
+    }
+}
