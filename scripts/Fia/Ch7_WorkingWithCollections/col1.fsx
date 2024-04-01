e29acc32e88fad57829964607bf9e6052214c06b
tony <antonel.pazargic@gmail.com>
2024-04-01 22:29:41 +0300

    simple collections

---
+++ scripts/Fia/Ch7_WorkingWithCollections/col1.fsx
@@ -0,0 +1,10 @@
+(* list *)
+
+let firstNumbers = [1; 2; 3; 4; 5]
+let lastNumbers = [6; 7; 8; 9; 10]
+
+(* working with immutable lists *)
+let allNumbers = firstNumbers @ lastNumbers
+
+let preparedZero = 0 :: allNumbers
+
