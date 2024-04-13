f1ea85b52e5b0befa61ae9ad6d17e9f363e00082
tony <antonel.pazargic@gmail.com>
2024-04-13 13:06:07 +0300

    Queue call center

---
+++ scripts/datastructures_algorithms/da_queue_callcenter.fsx
@@ -0,0 +1,83 @@
+
+open System
+open System.Collections.Generic
+
+
+// ----------------- 1.1 -----------------
+// model definition
+// ---------------------------------------
+
+module Domain =
+    
+    type Consultant = | Consultant of string
+
+    type IncomingCall = {
+        Id: int
+        ClientId: int
+        CallTime: DateTime
+        AnswerTime: DateTime option
+        EndTime: DateTime option
+        Consultant: Consultant
+    }
+
+    type CallCenter = {
+        mutable Counter: int
+        Calls: Queue<IncomingCall>
+    }
+
+// ----------------- 1.2 -----------------
+// behaviour definition
+// ---------------------------------------
+
+open Domain
+
+module CallCenter =
+    let call clientId consultant callCenter =
+        callCenter.Counter <- callCenter.Counter + 1
+        let newCall =
+            { Id = callCenter.Counter + 1
+              ClientId = clientId
+              CallTime = DateTime.Now
+              AnswerTime = None
+              EndTime = None
+              Consultant  =  consultant }    
+        callCenter.Calls.Enqueue newCall
+        printfn $"Call from {clientId} at {newCall.CallTime} assigned to {consultant} with id {newCall.Id}"
+        callCenter
+
+    let areWaitingCalls callCenter =
+        callCenter.Calls.Count > 0
+    
+    let answerCall consultant callCenter =
+        let call = { callCenter.Calls.Dequeue() with AnswerTime = Some DateTime.Now; Consultant = consultant }
+        printfn $"Answered call {call.Id} from {call.ClientId} answered by {consultant} at {call.AnswerTime}"
+        call
+
+    let endCall call =
+        let endedCall = { call with EndTime = Some DateTime.Now }
+        printfn $"Ended call %A{endedCall}"
+
+// ----------------- 1.3 -----------------
+// app
+// ---------------------------------------
+
+open CallCenter
+let app =
+    let callCenter = { Counter = 0
+                       Calls = Queue() }
+    let consultant = Consultant "Tony"
+    callCenter
+        |> call 1234 consultant
+        |> call 5678 consultant
+        |> call 1468 consultant
+        |> call 9641 consultant
+        |> ignore
+    
+    
+    let rec loop callCenter =
+        if areWaitingCalls callCenter then
+            callCenter |> answerCall consultant |> endCall; loop callCenter
+        else
+            callCenter
+            
+    loop callCenter |> ignore
