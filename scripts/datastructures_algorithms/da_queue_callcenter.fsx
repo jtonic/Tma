a80a206c2402d575d3f9d2792f73ec93d22fe10f
tony <antonel.pazargic@gmail.com>
2024-04-13 17:36:02 +0300

    Asynchronously processing the queued calls in the CallCenter with a single operator.

--- scripts/datastructures_algorithms/da_queue_callcenter.fsx
+++ scripts/datastructures_algorithms/da_queue_callcenter.fsx
@@ -1,16 +1,10 @@
-
 open System
 open System.Collections.Generic
-
-
 // ----------------- 1.1 -----------------
 // model definition
 // ---------------------------------------
-
 module Domain =
-    
     type Consultant = | Consultant of string
-
     type IncomingCall = {
         Id: int
         ClientId: int
@@ -19,23 +13,19 @@ module Domain =
         EndTime: DateTime option
         Consultant: Consultant
     }
-
     type CallCenter = {
         mutable Counter: int
         Calls: Queue<IncomingCall>
     }
-
 // ----------------- 1.2 -----------------
 // behaviour definition
 // ---------------------------------------
-
 open Domain
-
 module CallCenter =
     let call clientId consultant callCenter =
         callCenter.Counter <- callCenter.Counter + 1
         let newCall =
-            { Id = callCenter.Counter + 1
+            { Id = callCenter.Counter
               ClientId = clientId
               CallTime = DateTime.Now
               AnswerTime = None
@@ -44,40 +34,38 @@ module CallCenter =
         callCenter.Calls.Enqueue newCall
         printfn $"Call from {clientId} at {newCall.CallTime} assigned to {consultant} with id {newCall.Id}"
         callCenter
-
     let areWaitingCalls callCenter =
         callCenter.Calls.Count > 0
-    
     let answerCall consultant callCenter =
         let call = { callCenter.Calls.Dequeue() with AnswerTime = Some DateTime.Now; Consultant = consultant }
         printfn $"Answered call {call.Id} from {call.ClientId} answered by {consultant} at {call.AnswerTime}"
         call
-
     let endCall call =
         let endedCall = { call with EndTime = Some DateTime.Now }
         printfn $"Ended call %A{endedCall}"
-
 // ----------------- 1.3 -----------------
 // app
 // ---------------------------------------
-
 open CallCenter
 let app =
-    let callCenter = { Counter = 0
-                       Calls = Queue() }
+    let callCenter = { Counter = 0; Calls = Queue() }
     let consultant = Consultant "Tony"
     
-    let rec operate callCenter =
+    let rec processCalls callCenter: Async<CallCenter> = async {
         if areWaitingCalls callCenter then
+            let rand = Random()
+            do! Async.Sleep (rand.Next (500, 1000))
             callCenter |> answerCall consultant |> endCall
-            operate callCenter
+            return! processCalls callCenter
         else
-            callCenter
-            
+            return callCenter
+        }
+    
     callCenter
         |> call 1234 consultant
         |> call 5678 consultant
         |> call 1468 consultant
         |> call 9641 consultant
-        |> operate
+        |> processCalls
+        |> Async.RunSynchronously
         |> ignore
